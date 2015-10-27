e8f484e9448f827a4a1c1d47c548608b738d06f9
chuchu-beleza <chuchu-beleza@pa1-58da05.intra.if.usp.br>
2015-10-27 00:46:31 -0200

    sync

--- DataAnalizing.py
+++ DataAnalizing.py
@@ -5,16 +5,30 @@ import sys
 sys.path.insert(0, '../GeneralMonteCarlo')
 import _SQLite_Database_ as SQLite
 
-Data = SQLite._SQLite_Database_('DCP_new.db', 'DilutedContact')
-PrimaryKey = []
-PrimaryKey.append([128.0,1.64872,1.0, 500.0, 500.0,2.0])
-PrimaryKey.append([256.0,1.64872,1.0,1000.0,1000.0,2.0])
-Delta = [       1.0,0.00001,0.1,    0.1,   0.1,0.1]
+DatabaseName = ['DCP.db', 'DCP_new.db']
 
-for x in PrimaryKey:
+#Data = SQLite._SQLite_Database_('DCP_new.db', 'DilutedContact')
+PrimaryKey = []
+#PrimaryKey.append([128.0,1.64872,1.0,    500.0, 500.0,2.0])
+PrimaryKey.append([256.0,1.64872,1.0,   1000.0,   1000.0  ,2.0])
+Delta = [          500.0,0.00001,0.1,1000000.0,1000000.0,500.0]
+
+for x in DatabaseName:
+	print x
+	Data = SQLite._SQLite_Database_(x, 'DilutedContact')
+	y = Data.SearchforAll(PrimaryKey[0], Delta)
+	for u in y:
+		rho2 = u[7]/u[13]
+		rho22 = u[8]/u[13]
+#		rho2 = u[7]
+#		rho22 = u[8]
+		rho2 = rho2*rho2
+		print u[0:7], (rho22-rho2)/rho2
+
+#for x in PrimaryKey:
 #	PrimaryKey[0] = x
-	u = Data.SearchforAll(x, Delta)
-	rho2 = u[0][7]/u[0][13]
-	rho2 = rho2*rho2
-	rho22 = u[0][8]/u[0][13]
-	print x, (rho22-rho2)/rho2
+#	u = Data.SearchforAll(x, Delta)
+#	rho2 = u[0][7]/u[0][13]
+#	rho2 = rho2*rho2
+#	rho22 = u[0][8]/u[0][13]
+#	print x, (rho22-rho2)/rho2
