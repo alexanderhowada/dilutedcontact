b133edfa4184cc8868c9a7c3f33ae640a3a8dd9d
Alexander Wada <qwerty@abaxng.if.usp.br>
2015-10-28 10:04:28 -0200

    sync

--- InputGen.cpp
+++ InputGen.cpp
@@ -17,16 +17,17 @@ int main(int Nargs, char *Input[]){
 
  *L = 32.0;
  *p = 1.0;
- *t_init = 5000.0;
- *NThermalMeans = 500.0;
+ *t_init = 100000.0;
+ *NThermalMeans = 100000.0;
  *tau = 2.0;
- *NDisConf = 200.0;
+ *NDisConf = 1000.0;
 
  FILE *save = fopen(SaveName, "a+");
  if(save == NULL) exit(1);
-
- for(*Lambda = 1.0; *Lambda < 3.0; *Lambda += 0.02){
+ for(*L = 64.0; *L < 257.0; (*L) *=2.0){
+ for(*Lambda = 1.64872 - 0.1; *Lambda < 1.64872 + 0.1; *Lambda += 0.01){
 	Inputs.Print(save, PrintSpec, " ");
 	fprintf(save, "\n");
+ }
  }
 }
