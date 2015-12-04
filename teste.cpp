9fc257c97aadb747babb3c6cb15937aa4a245e3a
chuchu-beleza <chuchu-beleza@pa1-58da05.intra.if.usp.br>
2015-12-04 20:12:00 -0200

    profile

--- teste.cpp
+++ teste.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
-#include<mpi.h>
+//#include<iostream>
+//#include<mpi.h>
 #include<time.h>
 #include"_2DDilutedContact_.h"
-#include"_Time_Series_.h"
-#include"_Parallelize_TimeSeries_.h"
+//#include"_Time_Series_.h"
+//#include"_Parallelize_TimeSeries_.h"
 
 int main(int Nargs, char *Input[]){
  int rank, size;
@@ -11,31 +11,23 @@ int main(int Nargs, char *Input[]){
  MPI_Comm_size(MPI_COMM_WORLD, &size);
  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
- _2DDilutedContact_ SiSimul("DCP_new.db", "DilutedContact");
- _Time_Series_ Res_t(SiSimul, 2);
-// unsigned int Res_size = SiSimul.P2_Res->Get_size();
+ _2DDilutedContact_ SiSimul(time(NULL));
 
-// for(unsigned int count = 0; count < size; count
- if(rank == 0){
-	Res_t.Res_Array.Realloc(SiSimul.Results.Get_size()*3);
-	for(int index = 0; index < Res_t.Res_Array.Get_size(); index++) Res_t.Res_Array[index] = index;
-	for(unsigned int index = 0; index < 6; index++) Res_t.Parameters[index] = ~index;
-//	Res_t.Set_to_0();
-	Res_t.SendAll(1, 0);
-	Res_t.Set_to_0();
-	Res_t.Res_Array.Realloc(SiSimul.Results.Get_size()*1);
-	Res_t.SendAll(1, 0);
+ FILE *Inputs = fopen("Inputs.txt", "r");
+ 
+ SiSimul.Parameters.Scan(Inputs, "%lf", "%*c");
+ SiSimul.Parameters.Print(stdout, "%lf", " ");
+ printf("\n");
+ unsigned long long NSimul = 1;
+// double tau = SiSimul.Parameters[5];
+ for(; NSimul; NSimul--){
+	SiSimul.Set_Parameters();
+	SiSimul.Set_InitialConditions();
+	for(SiSimul.Parameters[3] = 0.0; SiSimul.Parameters[3] < SiSimul.Parameters[4]; SiSimul.Parameters[3] += SiSimul.Parameters[5]){
+		SiSimul.Simulate();
+	}
  }
- else{
-	Res_t.SleepRecvAnyAll();
-	Res_t.Res_Array.Print(stdout, "%.0lf", " ");
-	printf("\n");
-	Res_t.Parameters.Print(stdout, "%lf", " ");
-	printf("\n");
-	Res_t.SleepRecvAnyAll();
-	Res_t.Res_Array.Print(stdout, "%.0lf", " ");
-	printf("\n");
- }
+
  MPI_Finalize();
  return 0;
 }
