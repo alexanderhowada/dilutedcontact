3614b1b7a7e29f5d9047a3f4dc5f6de04bc8afaf
Alexander Wada <qwerty@abax>
2015-12-21 17:04:10 -0200

    sync

--- run-timeseries.sh
+++ run-timeseries.sh
@@ -1,1 +1,1 @@
-mpirun -n 148 --hostfile nos.txt ./../2DDC-timeseries DCP-timeseries.db DilutedContact Inputs.txt
+mpirun -n 148 --hostfile nos.txt ./../2DDC-timeseries DCP-timeseries.db DilutedContact Inputs.txt > run-timeseries.txt
