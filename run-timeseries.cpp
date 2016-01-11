3303a3a212ec9bed738fa9d6729c0692e2a75d7e
Alexander Wada <qwerty@abax>
2016-01-11 14:18:58 -0200

    sync

--- run-timeseries.cpp
+++ run-timeseries.cpp
@@ -16,7 +16,7 @@ int main(int Nargs, char* Inputs[]){
  std::random_device SeedGen;
  _MPI_vector_<unsigned int> Seed(1);
 
- const unsigned int NSimul = 1UL<<10;
+ const unsigned int NSimul = 1UL<<12;
  if(rank == 0){
 	for(int process = 1; process < size; process++){
 		Seed[0] = SeedGen();
