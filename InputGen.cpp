829f2eb6d1c658afbcb4fecf8b7391e215f321d2
Alexander Wada <qwerty@abax>
2016-01-14 15:06:59 -0200

    sync

--- InputGen.cpp
+++ InputGen.cpp
@@ -16,17 +16,17 @@ int main(int Nargs, char *Input[]){
  const char *PrintSpec = "%lf";
  const char *SaveName = "Inputs2.txt";
 
- *L = 256.0;
+ *L = 128.0;
  *p = 1.0;
- *t_init = 500000.0;
- *NThermalMeans = 1000000.0;
+ *t_init = 100000.0;
+ *NThermalMeans = 500000.0;
  *tau = 5.0;
- *NDisConf = (1<<12);
+ *NDisConf = (1<<9);
 
  FILE *save = fopen(SaveName, "w");
  if(save == NULL) exit(1);
  for(*Lambda = 1.6; *Lambda < 5.0; *Lambda += 0.025){
- for(*p = 0.6; *p <= 1.0; *p+=0.025){
+ for(*p = 0.6; *p <= 1.001; *p+=0.025){
 		Inputs.Print(save, PrintSpec, " ");
 		fprintf(save, "\n");
  }
