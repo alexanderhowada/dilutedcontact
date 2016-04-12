62fa4225cb0f1f8724576ffa2ee9bd32a18a9b4e
chuchubeleza <chuchubeleza@pa1-033f0d.intra.if.usp.br>
2016-04-12 17:54:44 -0300

    sync

--- InputGen.cpp
+++ InputGen.cpp
@@ -19,15 +19,15 @@ int main(int Nargs, char *Input[]){
  *L = 1024.0;
  *p = 0.9;
  *t_init = 100.;
- *NThermalMeans = 1000000.;
+ *NThermalMeans = 10000.;
  *tau = 0.05;
- *NDisConf = 300000.;
+ *NDisConf = 200000.;
 
  FILE *save = fopen(SaveName, "a+");
  if(save == NULL) exit(1);
- for(*L=1024; *L<2049; *L*=2){
- for(*Lambda = 1.80; *Lambda < 2.01; *Lambda += 0.01){
- for(*p = 0.85; *p < 0.91; *p+=0.01){
+ for(*L=512; *L<1025; *L*=2){
+ for(*Lambda = 2.0; *Lambda < 2.33; *Lambda += 0.03){
+ for(*p = 0.75; *p < 0.80; *p+=0.01){
 		Inputs.Print(save, PrintSpec, " ");
 		fprintf(save, "\n");
  }
