f55f3928f6d6e687121221a3481221403431d6ab
chuchubeleza <chuchubeleza@pa1-033f0d.intra.if.usp.br>
2016-06-15 17:40:30 -0300

    sync

--- InputGen.cpp
+++ InputGen.cpp
@@ -17,20 +17,22 @@ int main(int Nargs, char *Input[]){
  const char *SaveName = "Inputspy.txt";
 
  *L = 1024.0;
- *p = 0.9;
- *t_init = 100.;
- *NThermalMeans = 10000.;
- *tau = 0.05;
- *NDisConf = 200000.;
+// *p = 0.592746050792;
+ *p = 1.0;
+ *t_init = 0.;
+ *NThermalMeans = 100000.;
+ *tau = 4.;
+// *tau = 1.0;
+ *NDisConf = 16.;
 
  FILE *save = fopen(SaveName, "a+");
  if(save == NULL) exit(1);
- for(*L=512; *L<1025; *L*=2){
- for(*Lambda = 3.11; *Lambda < 3.20; *Lambda += 0.01){
- for(*p = 0.59274605079210; *p < 0.60; *p+=0.001){
+ for(*L=1024; *L<2049; *L*=2){
+ for(*Lambda = 1.0; *Lambda < 2.1; *Lambda += 0.1){
+// for(*p = 0.592746050792; *p < 1.0; *p+=0.025){
 		Inputs.Print(save, PrintSpec, " ");
 		fprintf(save, "\n");
- }
+// }
  }
  }
 }
