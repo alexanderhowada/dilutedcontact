0cd6178904ad443546964f5c2f8a6bcc43189763
Alexander Wada <qwerty@abax>
2016-07-05 14:46:49 -0300

    sync

--- run-timeseriesFull.sh
+++ run-timeseriesFull.sh
@@ -1,1 +1,3 @@
 mpirun -n 27 --hostfile nos-bcdc.txt ./../2DDC-timeseriesFull DCP-timeseriesFull.db DilutedContact Inputspy.txt &> run-timeseriesFull.txt
+mpirun -n 126 --hostfile nos.txt ./../2DDC-timeseriesFull DCP-timeseriesFull005.db DilutedContact InputsTimeseriesFull.txt &> run-timeseriesFull005.txt
+mpirun -n 126 --hostfile nos.txt ./../2DDC-timeseriesFull DCP-timeseriesFull001.db DilutedContact InputsTimeseriesFull.txt &> run-timeseriesFull001.txt
